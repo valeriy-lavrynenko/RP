25fb3034dc645ea61913a24654f8f6586ecdd40c
valeriy.lavrynenko <valeriy.lavrynenko@gmail.com>
2017-10-03 18:58:10 +0300

    Removing globals

--- config/GLOBALS.js
+++
@@ -1,9 +0,0 @@
-const BrowserUtils = require('./../utils/browser.utils');
-const CommonUtils = require('./../utils/common.utils');
-
-module.exports = function () {
-    global.EC = protractor.ExpectedConditions;
-    global.utils = CommonUtils;
-    global.utils.wd = BrowserUtils;
-    global.consume = BrowserUtils.consume;
-};
